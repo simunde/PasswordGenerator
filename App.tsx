1e8af507d635eb58f60fff74918158d1a073beaa
simunde <sidmundece17@gmail.com>
2024-07-06 11:59:57 -0400

    Added title and Textinput box

--- App.tsx
+++ App.tsx
@@ -1,6 +1,9 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView, ScrollView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native'
 import React, { useState } from 'react'
 
+import BouncyCheckbox from "react-native-bouncy-checkbox";
+
+import {Formik } from 'formik'
 import * as YUP from 'yup'
 
 const PasswordSchema = YUP.object().shape({
@@ -73,10 +76,157 @@ export default function App() {
     }
 
   return (
-    <View>
-      <Text>Appddddd</Text>
-    </View>
+    <ScrollView keyboardShouldPersistTaps="handled">
+      <SafeAreaView>
+        <View style={styles.appContainer}>
+        <Text style={styles.title}>Password Generator</Text>
+
+        <Formik
+       initialValues={{passwordLength:'' }}
+       validationSchema={PasswordSchema}
+        onSubmit={values=>{
+
+          console.log(values);
+          
+          generatePasswordString((+values.passwordLength))
+        }}
+     >
+       {({
+         values,
+         errors,
+         touched,
+         isValid,
+         handleChange,
+         handleSubmit,
+         handleReset,
+         /* and other goodies */
+       }) => (
+          
+          <>
+          <View style={styles.inputWrapper}></View>
+          <View style={styles.inputColumn}>
+            <TextInput
+            style={styles.inputStyle}
+            value={values.passwordLength}
+            onChangeText={handleChange('passwordLength')}
+            placeholder='Ex. 8'
+            keyboardType='numeric'
+            />
+          </View>
+
+          <View style={styles.inputWrapper}></View>
+          <View style={styles.inputWrapper}></View>
+          <View style={styles.inputWrapper}></View>
+          <View style={styles.inputWrapper}></View>
+          <View style={styles.formActions}>
+            <TouchableOpacity>
+              <Text>Generate Password</Text>
+              </TouchableOpacity>
+            <TouchableOpacity>
+            <Text>Reset</Text>
+            </TouchableOpacity>
+          </View>
+          </>
+       )}
+     </Formik>
+
+        </View>
+      </SafeAreaView>
+    </ScrollView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  appContainer: {
+    flex: 1,
+  },
+  formContainer: {
+    margin: 8,
+    padding: 8,
+  },
+  title: {
+    fontSize: 32,
+    fontWeight: '600',
+    marginBottom: 15,
+  },
+  subTitle: {
+    fontSize: 26,
+    fontWeight: '600',
+    marginBottom: 2,
+  },
+  description: {
+    color: '#758283',
+    marginBottom: 8,
+  },
+  heading: {
+    fontSize: 15,
+  },
+  inputWrapper: {
+    marginBottom: 15,
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    flexDirection: 'row',
+  },
+  inputColumn: {
+    flexDirection: 'column',
+  },
+  inputStyle: {
+    padding: 8,
+    width: '30%',
+    borderWidth: 1,
+    borderRadius: 4,
+    borderColor: '#16213e',
+  },
+  errorText: {
+    fontSize: 12,
+    color: '#ff0d10',
+  },
+  formActions: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+  },
+  primaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#5DA3FA',
+  },
+  primaryBtnTxt: {
+    color: '#fff',
+    textAlign: 'center',
+    fontWeight: '700',
+  },
+  secondaryBtn: {
+    width: 120,
+    padding: 10,
+    borderRadius: 8,
+    marginHorizontal: 8,
+    backgroundColor: '#CAD5E2',
+  },
+  secondaryBtnTxt: {
+    textAlign: 'center',
+  },
+  card: {
+    padding: 12,
+    borderRadius: 6,
+    marginHorizontal: 12,
+  },
+  cardElevated: {
+    backgroundColor: '#ffffff',
+    elevation: 1,
+    shadowOffset: {
+      width: 1,
+      height: 1,
+    },
+    shadowColor: '#333',
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+  },
+  generatedPassword: {
+    fontSize: 22,
+    textAlign: 'center',
+    marginBottom: 12,
+    color:'#000'
+  },
+});
